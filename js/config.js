963ca1d9fe994f26b91270e4842456ad893d7078
charlesleviisoza <141874682+charlesleviisoza@users.noreply.github.com>
2025-12-22 23:35:34 -0600

    Update Cognito configuration in config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_f0rm3A450', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3hjik7j24n1sackvdci7t9behr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
