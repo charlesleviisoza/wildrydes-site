30b54dc4575b5826beb9f65d4f2161809884f169
charlesleviisoza <141874682+charlesleviisoza@users.noreply.github.com>
2025-12-22 23:54:18 -0600

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_f0rm3A450', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3hjik7j24n1sackvdci7t9behr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6n6b5khr9u57f5dbs2ooc2amg9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
